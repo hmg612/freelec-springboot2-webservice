91d60ce8d063d8a386f3c7a1afa13ace57424b1c
hmg612 <hmgcjn@gmail.com>
2020-07-15 14:54:05 +0900

    re-repository work (7/15)
    re-repository work (7/15)

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity  // Spring Security설정 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable() //h2-console 화면 사용을 위해 해당 옵션들 disable
+                .and()
+                    .authorizeRequests() //URL별 권한 관리를 설정하는 옵션 시작점 (antMatchers옵션 사용가능)
+                    .antMatchers("/", "/css/**", "/images/**",
+                            "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // 권한 관리 대상 지정
+                    .anyRequest().authenticated() // 설정된 값들 이외 나머지 URL(인증된 사용자에게만 URL접근 허용)
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
